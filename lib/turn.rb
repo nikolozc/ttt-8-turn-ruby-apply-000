618b3781b68560811b8342e2dbe03df044c8e64d
Nikoloz Chapichadze <nikoloz.chapichadze@gmail.com>
2020-09-24 02:59:00 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,41 @@
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  input = input_to_index(input)
+  until(valid_move?(board,input))
+    turn(board)
+  end
+end
+
+def valid_move?(board, index)
+    if (position_taken?(board,index) || !index.between?(0,8))
+        return false
+    else
+        return true
+    end
+end
+
+def position_taken?(board,index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else 
+    return true
+  end
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(num)
+  num.to_i - 1
+end
+
+def move(board,index,player = "X")
+  board[index] = player
+  return board
+end
